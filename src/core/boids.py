7665c9df1e1b834470c4941036e89ea28b0b6c29
Gabryss <gabriel.garcia@utbm.fr>
2021-11-12 07:29:17 +0100

    Add new methods (usual like: get_velocity, set_position)

--- src/core/boids.py
+++ src/core/boids.py
@@ -1,10 +1,9 @@
 """
 Boids file
 """
-from enums.colors import Color
 from enums.config import Config
 import random
-
+import pyglet
 
 def initialize_boids():
     pass
@@ -14,15 +13,38 @@ def initialize_boids():
 
 class Boid ():
 
-    def __init__(self, position_p=random.uniform(0,100), velocity_p=random.uniform(0,Config.MAXSPEED.value), size_p=10.0, color_p=Color.RED.value):
+    def __init__(self, position_p=random.uniform(0,100), velocity_p=random.uniform(0,Config.MAXSPEED.value), size_p=10.0, color_p=Config.DEFAULT_COLOR_RED.value):
         self.position = position_p
         self.velocity = velocity_p
         self.size = size_p
         self.color = color_p
+        self.shape = pyglet.shapes.Triangle(0,0, 20, 20, 20, 10, self.color, batch=Config.BATCH.value)
 
     def __repr__(self):
         return "Boid: position={}, velocity={}, color={}".format(
             self.position, self.velocity, self.color)
     
-
+    def get_size(self):
+        return self.size
+    
+    def get_color(self):
+        return self.color
+    
+    def get_velocity(self):
+        return self.velocity
+    
+    def get_position(self):
+        return self.position
+    
+    def set_size(self,size_p):
+        self.size = size_p
+    
+    def set_color(self, color_p):
+        self.color = color_p
+    
+    def set_velocity(self, velocity_p):
+        self.velocity = velocity_p
+    
+    def set_position(self, position_p):
+        self.position = position_p
     
