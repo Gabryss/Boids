d378a7157ca49086f7731bf1e0d11a322a9735f1
Gabryss <gabriel.garcia@utbm.fr>
2021-11-14 22:46:33 +0100

    Major update (-Replace triangle by polygons, -Implement rotation, -Implement rotation to face the direction, -Implement window edge limit, -Implement translation)

--- src/core/boids.py
+++ src/core/boids.py
@@ -1,29 +1,30 @@
 """
 Boids file
 """
+import math
 from enums.config import Config
 import random
 import pyglet
+import boot
+import numpy as np
 
-def initialize_boids(nb=20):
-    boids_list=[]
-    for i in range(20):
-        boids_list.append(Boid())
-    return boids_list
-
+def initialize_boid(width, height):
+    return(Boid(position_p=[random.uniform(0,width), random.uniform(0,height)], velocity_p=[random.uniform(-20,20), random.uniform(-20,20)]))
 
 
+class Boid():
 
-class Boid ():
-
-    def __init__(self, position_p=random.uniform(0,100), velocity_p=random.uniform(0,Config.MAXSPEED.value), size_p=3, color_p=Config.DEFAULT_COLOR_RED.value):
+    def __init__(self, position_p=[random.uniform(0,500),random.uniform(0,500)], velocity_p=[random.uniform(-20,20), random.uniform(-20,20)], size_p=3, color_p=Config.DEFAULT_COLOR_RED.value):
         self.position = position_p
         self.velocity = velocity_p
         self.size = size_p
         self.color = color_p
+        self.time = 0
+        self.batch = Config.BATCH.value
 
-        #Triangle shape parameters : p1(x), p1(y), p2(x), p2(y), p3(x), p3(y)
-        self.shape = pyglet.shapes.Triangle(self.position+(2*self.size),self.position, self.position-self.size, self.position+self.size, self.position-self.size, self.position-self.size, self.color, batch=Config.BATCH.value)
+        #Boid shape parameters : center_point [x1,y1], lower_wing [x2,y2], noze [x3,y3], upper_wing[x4,y4], color, batch
+        self.shape = pyglet.shapes.Polygon([self.position[0],self.position[1]],[self.position[0]-self.size,self.position[1]-self.size],[self.position[0]+2*self.size,self.position[1]],[self.position[0]-self.size,self.position[1]+self.size],color=self.color, batch=Config.BATCH.value)
+
 
     def __repr__(self):
         return "Boid: position={}, velocity={}, color={}".format(
@@ -52,4 +53,66 @@ class Boid ():
     
     def set_position(self, position_p):
         self.position = position_p
+    
+    def nearby_boids(self,boids):
+        pass
+    
+    def _rotate_shape(self):
+        """Rotate base image using the velocity and assign to image."""
+        angle = -np.rad2deg(np.angle(self.velocity[0] + 1j * self.velocity[1]))
+        self.shape.rotation = angle
+
+    
+    def update(self, delta_time):
+        self.time += delta_time
+
+        self.shape.x += self.velocity[0] * delta_time
+        self.shape.y += self.velocity[1] * delta_time
+
+        self.shape.x1 += self.velocity[0] * delta_time
+        self.shape.y1 += self.velocity[1] * delta_time
+
+        self.shape.x2 += self.velocity[0] * delta_time
+        self.shape.y2 += self.velocity[1] * delta_time
+
+        self.shape.x3 += self.velocity[0] * delta_time
+        self.shape.y3 += self.velocity[1] * delta_time
+
+        self._rotate_shape()
+
+        if self.shape.x > boot.window.width:
+            self.shape.x = 0
+
+            self.shape.x1 = -self.size
+
+            self.shape.x2 = 2*self.size
+
+            self.shape.x3 = -self.size
+        
+        if self.shape.x < 0:
+            self.shape.x = boot.window.width
+
+            self.shape.x1 = boot.window.width - self.size
+
+            self.shape.x2 = boot.window.width + 2*self.size
+
+            self.shape.x3 = boot.window.width - self.size
+
+        if self.shape.y > boot.window.height:
+            self.shape.y = 0
+
+            self.shape.y1 = -self.size
+
+            self.shape.y2 = 0
+
+            self.shape.y3 = +self.size
+        
+        if self.shape.y < 0:
+            self.shape.y = boot.window.height
+
+            self.shape.y1 = boot.window.height - self.size
+
+            self.shape.y2 = boot.window.height
+
+            self.shape.y3 = boot.window.height + self.size
     
