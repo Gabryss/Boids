33fae891e7c77e0bb7ea8bf1fb50069766926ca8
Gabryss <gabriel.garcia@utbm.fr>
2021-11-14 22:47:55 +0100

    -Implement wall shape, -Velocity is now a 2D vector

--- src/core/walls.py
+++ src/core/walls.py
@@ -3,14 +3,18 @@ Wall file
 """
 from enums.config import Config
 import random
+import pyglet
 
 
 class Wall ():
 
-    def __init__(self, position_p=random.uniform(0,100), size_p=10.0, color_p=Config.DEFAULT_COLOR_RED.value):
+    def __init__(self, position_p=[random.uniform(0,500),random.uniform(0,500)], width_p = 50, height_p=10.0, color_p=Config.DEFAULT_COLOR_RED.value):
         self.position = position_p
-        self.size = size_p
+        self.width = width_p
+        self.height = height_p
         self.color = color_p
+
+        self.shape = pyglet.shapes.Rectangle(self.position[0],self.position[1], width_p, height_p, color=self.color, batch=Config.BATCH.value)
 
     def __repr__(self):
         return "Wall: position={}, color={}".format(
