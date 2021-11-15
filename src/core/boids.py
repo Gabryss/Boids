e099cf16810bc24408974c36f0e92764075c0bc5
Gabryss <gabriel.garcia@utbm.fr>
2021-11-15 07:16:06 +0100

    -Clues to fix the 'translation' issue

--- src/core/boids.py
+++ src/core/boids.py
@@ -1,7 +1,7 @@
 """
 Boids file
 """
-import math
+from pyglet.shapes import Polygon,_ShapeBase
 from enums.config import Config
 import random
 import pyglet
@@ -12,7 +12,7 @@ def initialize_boid(width, height):
     return(Boid(position_p=[random.uniform(0,width), random.uniform(0,height)], velocity_p=[random.uniform(-20,20), random.uniform(-20,20)]))
 
 
-class Boid():
+class Boid(Polygon):
 
     def __init__(self, position_p=[random.uniform(0,500),random.uniform(0,500)], velocity_p=[random.uniform(-20,20), random.uniform(-20,20)], size_p=3, color_p=Config.DEFAULT_COLOR_RED.value):
         self.position = position_p
@@ -21,6 +21,8 @@ class Boid():
         self.color = color_p
         self.time = 0
         self.batch = Config.BATCH.value
+        self._coordinates = list([self.position[0],self.position[1]],[self.position[0]-self.size,self.position[1]-self.size],[self.position[0]+2*self.size,self.position[1]],[self.position[0]-self.size,self.position[1]+self.size])
+        super().__init__(coordinates=self._coordinates,color=self.color, batch=Config.BATCH.value)
 
         #Boid shape parameters : center_point [x1,y1], lower_wing [x2,y2], noze [x3,y3], upper_wing[x4,y4], color, batch
         self.shape = pyglet.shapes.Polygon([self.position[0],self.position[1]],[self.position[0]-self.size,self.position[1]-self.size],[self.position[0]+2*self.size,self.position[1]],[self.position[0]-self.size,self.position[1]+self.size],color=self.color, batch=Config.BATCH.value)
@@ -56,6 +58,84 @@ class Boid():
     
     def nearby_boids(self,boids):
         pass
+    
+    @property
+    def x1(self):
+        """X coordinate of the shape.
+
+        :type: int or float
+        """
+        return self._coordinates[1][0]
+
+    @x1.setter
+    def x1(self, value):
+        self._coordinates[1][0] = value
+        self._update_position()
+    
+    @property
+    def x2(self):
+        """X coordinate of the shape.
+
+        :type: int or float
+        """
+        return self._coordinates[2][0]
+    
+    @x2.setter
+    def x2(self, value):
+        self._coordinates[2][0] = value
+        self._update_position()
+    
+    @property
+    def x3(self):
+        """X coordinate of the shape.
+
+        :type: int or float
+        """
+        return self._coordinates[3][0]
+    
+    @x3.setter
+    def x3(self, value):
+        self._coordinates[3][0] = value
+        self._update_position()
+    
+    @property
+    def y1(self):
+        """Y coordinate of the shape.
+
+        :type: int or float
+        """
+        return self._coordinates[1][1]
+
+    @y1.setter
+    def y1(self, value):
+        self._coordinates[1][1] = value
+        self._update_position()
+    
+    @property
+    def y2(self):
+        """Y coordinate of the shape.
+
+        :type: int or float
+        """
+        return self._coordinates[2][1]
+    
+    @y2.setter
+    def y2(self, value):
+        self._coordinates[2][1] = value
+        self._update_position()
+    
+    @property
+    def y3(self):
+        """Y coordinate of the shape.
+
+        :type: int or float
+        """
+        return self._coordinates[3][1]
+
+    @y3.setter
+    def y3(self, value):
+        self._coordinates[3][1] = value
+        self._update_position()
     
     def _rotate_shape(self):
         """Rotate base image using the velocity and assign to image."""
