0993444773ad15449bd507b6faaa8ebbc25b674d
Gabryss <gabriel.garcia@utbm.fr>
2022-11-29 16:38:41 +0100

    Add bounce on boundaries and fix the 'lagging' issue with a *dt on the velocity equation

--- src/core/boids.py
+++ src/core/boids.py
@@ -1,8 +1,6 @@
 """
 Boids core file
 """
-from distutils.command.config import config
-from pyglet.shapes import Polygon,_ShapeBase
 from enums.config import Config
 import random
 import pyglet
@@ -11,7 +9,7 @@ import rules
 import utils
 
 def initialize_boid(width, height):
-    return(Boid(position_p=[random.uniform(0,width), random.uniform(0,height)], velocity_p=[random.uniform(-20,20), random.uniform(-20,20)]))
+    return(Boid(position_p=[random.uniform(0,width), random.uniform(0,height)], velocity_p=[random.uniform(-Config.MAXSPEED.value,Config.MAXSPEED.value), random.uniform(-Config.MAXSPEED.value,Config.MAXSPEED.value)]))
 
 
 class Boid():
@@ -26,13 +24,11 @@ class Boid():
         self.bounds = [[self.position[0],self.position[1]],[self.position[0]-self.size,self.position[1]-self.size],[self.position[0]+2*self.size,self.position[1]],[self.position[0]-self.size,self.position[1]+self.size]]
         self.batch = Config.BATCH.value
         self.nearby_boids = []
-
-        # self.image = pyglet.image.load("/home/ggarcia/Documents/Boids/src/core/boid2.png")
-        # self.image.anchor_x = self.image.width // 2
-        # self.image.anchor_y = self.image.height // 2
-        # self.image.blit(self.position[0], self.position[1])
         #Boid shape parameters : center_point [x1,y1], lower_wing [x2,y2], noze [x3,y3], upper_wing[x4,y4], color, batch
         self.shape = pyglet.shapes.Polygon(self.bounds[0],self.bounds[1],self.bounds[2],self.bounds[3],color=self.color, batch=Config.BATCH.value)
+        self.shape.x = self.position[0]
+        self.shape.y = self.position[1]
+        self.shape.rotation = random.uniform(0,360)
 
 
     
@@ -43,12 +39,8 @@ class Boid():
         """
         nearby_boids = []
         for boid in boids:
-            # print(type(boid.get_position()), type(self.position))
-            # diff = boid.get_position() - self.position
             diff = [other_boid - this_boid for other_boid, this_boid in zip(boid.get_position(), self.position)]
-            # diff = (boid.position[0] - self.position[0], boid.position[1] - self.position[1])
 
-            # print(diff,"diff")
             if (boid != self and
                     utils.magnitude(*diff) <= Config.DEFAULT_ALIGNMENT_DIST.value and
                     utils.angle_between(self.velocity, diff) <= Config.VISIBLE_ANGLE.value):
@@ -64,65 +56,64 @@ class Boid():
         # self.nearby_boids = nearby_boids
         # return nearby_boids
 
+
+
+    def distance_boids(self, boids):
+        """
+        Calculate the distance between boids.
+        """
+        for boid in boids:
+            if boid != self:
+                distance = np.linalg.norm(boid.get_position() - self.position)
+                if distance <= Config.MAXDIST.value:
+                    self.nearby_boids.append(boid)
+        return self.nearby_boids
+    
+    def _can_see(self, boid):
+        """
+        Check if the boid can see another boid.
+        """
+        diff = boid.get_position() - self.position
+        return (boid != self and
+                np.linalg.norm(diff) <= Config.MAXDIST.value and
+                utils.angle_between(self.velocity, diff) <= Config.VISIBLE_ANGLE.value)
+
     
     def _rotate_shape(self):
         """
         Rotate base image using the velocity and assign to image.
         """
-        angle = -np.rad2deg(np.angle(self.velocity[0] + 1j * self.velocity[1]))
+        # angle = -np.rad2deg(np.angle(self.velocity[0] + 1j * self.velocity[1]))
+        angle = -np.rad2deg(np.arctan2(self.velocity[1], self.velocity[0]))
+
         self.shape.rotation = angle
 
 
     def _edge(self):
         """
         Check if the boid is outside the window and if so, reflect it.
-        If the boid touch a border of the window, it will respawn in the opposite border.
+        If the boid touch a border of the window, it will bounce on it.
         """       
-
-        if self.shape.x > Config.WINDOW_WIDTH.value:
-            self.shape.x = 0
-
-            self.shape.x1 = -self.size
-
-            self.shape.x2 = 2*self.size
-
-            self.shape.x3 = -self.size
-
-            self.position = (0, self.shape.y)
+        if self.shape.position[0] >= Config.WINDOW_WIDTH.value:
+            self.shape.position = Config.WINDOW_WIDTH.value, self.shape.position[1]
+            self.velocity = -self.velocity
+            self.acceleration = -self.acceleration
         
-        if self.shape.x < 0:
-            self.shape.x = Config.WINDOW_WIDTH.value
+        if self.shape.position[1] <= 0:
+            self.shape.position = self.shape.position[0], 0
+            self.velocity = -self.velocity
+            self.acceleration = -self.acceleration
 
-            self.shape.x1 = Config.WINDOW_WIDTH.value - self.size
-
-            self.shape.x2 = Config.WINDOW_WIDTH.value + 2*self.size
-
-            self.shape.x3 = Config.WINDOW_WIDTH.value - self.size
-
-            self.position = (Config.WINDOW_WIDTH.value, self.shape.y)
-
-        if self.shape.y > Config.WINDOW_HEIGHT.value:
-            self.shape.y = 0
-
-            self.shape.y1 = -self.size
-
-            self.shape.y2 = 0
-
-            self.shape.y3 = +self.size
-
-            self.position = (self.shape.x, 0)
+        if self.shape.position[1] >= Config.WINDOW_HEIGHT.value:
+            self.shape.position = self.shape.position[0], Config.WINDOW_HEIGHT.value
+            self.velocity = -self.velocity
+            self.acceleration = -self.acceleration
 
         
-        if self.shape.y < 0:
-            self.shape.y = Config.WINDOW_HEIGHT.value
-
-            self.shape.y1 = Config.WINDOW_HEIGHT.value - self.size
-
-            self.shape.y2 = Config.WINDOW_HEIGHT.value
-
-            self.shape.y3 = Config.WINDOW_HEIGHT.value + self.size
-
-            self.position = (self.shape.x, Config.WINDOW_HEIGHT.value)
+        if self.shape.position[0] <= 0:
+            self.shape.position = 0, self.shape.position[1]
+            self.velocity = -self.velocity
+            self.acceleration = -self.acceleration
     
 
     def _apply_forces(self, boids):
@@ -132,17 +123,19 @@ class Boid():
         alignment_vector = rules.alignment(self,boids)
         cohesion_vector = rules.cohesion(self,boids)
         separation_vector = rules.separation(self,boids)
+        centripete_vector = rules.centripete(self)
         self.forces = [ 
-                        # (Config.DEFAULT_ALIGNMENT_FORCE.value, alignment_vector),
+                        (Config.DEFAULT_ALIGNMENT_FORCE.value, alignment_vector),
                         (Config.DEFAULT_COHESION_FORCE.value, cohesion_vector),
                         (Config.DEFAULT_SEPARATION_FORCE.value, separation_vector),
+                        # (0.02, centripete_vector),
                         ]
         
         for force, vector in self.forces:
             if force and vector is not None:
                 self.acceleration += vector * force
 
-        self.velocity += self.acceleration
+        self.velocity += self.acceleration * self.time
         
 
     def _limit_velocity(self):
@@ -157,19 +150,10 @@ class Boid():
         """
         Update the boid's position and velocity.
         """
-        self.time += delta_time_p
-        
+        self.time = delta_time_p
         self.shape.x += self.velocity[0] * delta_time_p
         self.shape.y += self.velocity[1] * delta_time_p
 
-        self.shape.x1 += self.velocity[0] * delta_time_p
-        self.shape.y1 += self.velocity[1] * delta_time_p
-        
-        self.shape.x2 += self.velocity[0] * delta_time_p
-        self.shape.y2 += self.velocity[1] * delta_time_p
-
-        self.shape.x3 += self.velocity[0] * delta_time_p
-        self.shape.y3 += self.velocity[1] * delta_time_p
         
 
     def update(self, delta_time_p):
@@ -187,6 +171,9 @@ class Boid():
         self._apply_forces(self.nearby_boids)
 
         self._limit_velocity()
+
+        self.position = np.array([self.shape.x,self.shape.y])
+
 
 
 
