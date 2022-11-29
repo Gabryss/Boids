874c5f4f04f2bec29559c331ab4e8968452b30cd
Gabryss <gabriel.garcia@utbm.fr>
2022-11-29 16:42:26 +0100

    Centralise with the previous boot.py file

--- src/core/simulate.py
+++ src/core/simulate.py
@@ -1,10 +1,53 @@
 """
 Simulation file
-"""
 
+Initialize and start the simulation
+
+1-Initialize the window
+2-Initialize graphics batched rendering (for optimized performance)
+3-Start the simulation
+
+
+
+
+##TO DO
+    -Update window width and height when resized
+    -Boids rules
+    -Nearby boids detection
+    -Walls
+    -Translation without changing the pyglet library
+    -GUI
+"""
+import pyglet
 import boids
 from enums.config import Config
 
+
+
+""" 
+Initialize the window
+"""
+
+window = pyglet.window.Window(
+            fullscreen = False,
+            height = Config.WINDOW_HEIGHT.value,
+            width = Config.WINDOW_WIDTH.value,
+            resizable = True,
+            caption = "Boid simulation",
+        )
+batch = Config.BATCH.value
+
+@window.event
+def on_draw():
+    window.clear()
+    batch.draw()
+
+
+
+
+"""
+Start the simulation
+"""
 
 
 class Simulate():
@@ -31,3 +74,27 @@ class Simulate():
             """Animate the shapes"""
             self.time += delta_time
             self.square.rotation = self.time * 15
+
+
+
+
+
+if __name__ == "__main__":
+
+    simulation = Simulate()
+    simulation.init()
+
+    def update(dt):
+        # Update the simulation
+        for i in range(len(simulation.boids_list)):
+            simulation.boids_list[i].update(dt)
+        for i in range(len(simulation.boids_list)):
+            simulation.get_boids(accuracy="nearby")
+
+    # Update the game 60 times per second
+    # pyglet.clock.schedule_interval(update, 1 / 30.0)
+    pyglet.clock.schedule(update)
+        
+
+    # Run the app
+    pyglet.app.run()
